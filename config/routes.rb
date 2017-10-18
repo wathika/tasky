c8142d476fba1b9241e691cbfcffeb9d98a72b33
wathika <vcwathika@gmail.com>
2017-10-18 15:17:38 +0300

    configured task resource and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  resources :tasks, except: [:show]
 
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
+  root to: "tasks#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
