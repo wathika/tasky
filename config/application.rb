f1815f4ef2af0dff5b56190b7e432426c1160c44
wathika <vcwathika@gmail.com>
2017-10-19 00:07:08 +0300

    added a bunch of other required frameworks (except active_record)for rails5

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,18 @@
 require_relative 'boot'
 
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require "rails"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 module Tasky
   class Application < Rails::Application
